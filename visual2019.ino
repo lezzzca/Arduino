0e0ba6e4255a48d2ec9729156e7b227e94f4b6c1
lezzzca <lezzzca@gmail.com>
2019-05-23 18:18:55 -0300

    Primer commit

---
+++ visual2019.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       visual2019.ino
+    Created:	23/5/2019 18:05:59
+    Author:     LAPTOP-TEP8ES4O\lezzz
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
